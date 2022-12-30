d27ec93d0e365c82bd43ffa7ee999dc0ce295468
Marharyta Tchekotovskaia <marharyta_tchekotovskaia@epam.com>
2022-12-30 16:24:10 +0100

    fix Searching By Tag Test

--- src/test/java/com/epam/mentoring/taf/SearchingByTagTest.java
+++ src/test/java/com/epam/mentoring/taf/SearchingByTagTest.java
@@ -4,50 +4,60 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.*;
 
 public class SearchingByTagTest extends AbstractTest {
 
+    private static final String ARTICLES_BY_TAG_URL = "/api/articles?tag={tag}&limit=10&offset=0";
+    public static final String INVALID_TAG = "invalid_tag_name";
+    public static final String TAG_LIST_JSON_PATH = "articles.tagList";
+    public static final String ARTICLES_COUNT_JSON_PATH = "articlesCount";
+    public static final String APP_ARTICLE_PREVIEW_XPATH = "//div[@class='app-article-preview' and not(@hidden)]";
+    public static final String TAG_PILL_XPATH = "//a[contains(@class,'tag-pill')]";
+    public static final String NAV_LINK_XPATH = "//a[@class='nav-link active']";
+
+    @DataProvider(name = "apiDataProvider")
+    public Object[][] apiDataProviderMethod() {
+        return new Object[][] {{"implementations"},{"welcome"},{"ipsum"}};
+    }
     @Test
     public void uiVerification() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(@class,'tag-pill')]")));
-
-        int randomTag = (int) (Math.random() * 25);
-        WebElement tag = driver.findElement(By.xpath("//a[contains(@class,'tag-pill')][" + randomTag + "]"));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(TAG_PILL_XPATH)));
+        int tagCount = driver.findElements(By.xpath(TAG_PILL_XPATH)).size();
+        int randomTag = (int) (Math.random() * tagCount + 1);
+        WebElement tag = driver.findElement(By.xpath(TAG_PILL_XPATH + "[" + randomTag + "]"));
         String tagName = tag.getText();
         tag.click();
 
-        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class='app-article-preview' and not(@hidden)]")));
-        String selectedTag = driver.findElement(By.xpath("//a[@class='nav-link active']")).getText();
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(APP_ARTICLE_PREVIEW_XPATH)));
+        String selectedTag = driver.findElement(By.xpath(NAV_LINK_XPATH)).getText();
         Assert.assertEquals(selectedTag, tagName);
     }
 
-    @Test
-    public void apiVerification1() {
-        String tag = "test";
-        given().baseUri(API_URL).when().get("/api/articles?tag={tag}&limit=10&offset=0", tag).then().statusCode(200).body("articles.tagList", hasItem("test"));
-    }
-
-    @Test
-    public void apiVerification2() {
-        String tag = "simple";
-        given().baseUri(API_URL).when().get("/api/articles?tag={tag}&limit=10&offset=0", tag).then().statusCode(200).body("articles.tagList", hasItem("simple"));
-    }
-
-    @Test
-    public void apiVerification3() {
-        String tag = "cypress";
-        given().baseUri(API_URL).when().get("/api/articles?tag={tag}&limit=10&offset=0", tag).then().statusCode(200).body("articles.tagList", hasItem("cyprus"));
+    @Test(dataProvider = "apiDataProvider")
+    public void apiVerification(String tag) {
+        given()
+                .baseUri(API_URL)
+                .when()
+                .get(ARTICLES_BY_TAG_URL, tag)
+                .then()
+                .statusCode(200)
+                .body(TAG_LIST_JSON_PATH, everyItem(hasItem(tag)));
     }
 
     @Test
     public void apiNegativeVerification() {
-        String tag = "invalid_tag_name";
-        given().baseUri(API_URL).when().get("/api/articles?tag={tag}&limit=10&offset=0", tag).then().statusCode(200).body("articlesCount", equalTo(0));
+        given()
+                .baseUri(API_URL)
+                .when()
+                .get(ARTICLES_BY_TAG_URL, INVALID_TAG)
+                .then()
+                .statusCode(200)
+                .body(ARTICLES_COUNT_JSON_PATH, equalTo(0));
     }
 
 }
