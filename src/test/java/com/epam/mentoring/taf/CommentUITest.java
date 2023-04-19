c9f7bcdb0240e09a5797de143d0c221a52e8471c
Tatsiana Padbiarozkina <63b2d3c2741248746bf74127@bots.bitbucket.org>
2023-04-19 18:29:32 +0200

    Small changes related to AbstractTest

--- src/test/java/com/epam/mentoring/taf/CommentUITest.java
+++ src/test/java/com/epam/mentoring/taf/CommentUITest.java
@@ -1,17 +1,11 @@
 package com.epam.mentoring.taf;
 
-import com.epam.mentoring.taf.data.UserData;
-import com.epam.mentoring.taf.data.UserDataDTO;
-import com.epam.mentoring.taf.exception.ConfigurationSetupException;
 import com.epam.mentoring.taf.listeners.ReportPortalTestListener;
 import com.epam.mentoring.taf.listeners.TestListener;
 import io.qameta.allure.*;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-
-import java.io.IOException;
 
 @Listeners({TestListener.class, ReportPortalTestListener.class})
 @Feature("UI: Comments Tests")
@@ -20,26 +14,6 @@ public class CommentUITest extends AbstractTest {
     public static final String ALL_COMMENT = "ALL_COMMENT";
     public static final String ERROR_MESSAGE = "body can't be blank";
     public static final String COMMENT = "Test";
-
-    private UserDataDTO adminUser;
-
-    @BeforeMethod(description = "Authorization as Admin User")
-    public void authorizationAsAdminUser() {
-        try {
-            adminUser = UserData.getUserDataFromYaml("adminUser");
-        } catch (IOException e) {
-            throw new ConfigurationSetupException("Can't load default user data", e);
-        }
-    }
-
-    public void login() {
-        loginPage.clickSignInLink()
-                .fillInEmail(adminUser.getUserEmail())
-                .fillInPassword(adminUser.getUserPassword())
-                .clickSignInBtn();
-        homePage.navToUser();
-        userProfilePage.selectArt();
-    }
 
     @Test(description = "UI: add comment to article", priority = 1, groups = {"regression"})
     @Severity(SeverityLevel.BLOCKER)
@@ -75,13 +49,8 @@ public class CommentUITest extends AbstractTest {
         login();
         articlePage.deleteComment();
 
-        Assert.assertTrue(articlePage.commentIsNotDisplayed());
+        Assert.assertFalse(articlePage.commentIsNotDisplayed());
         logOut();
-    }
-
-    private static void logOut() {
-        homePage.navToSetting();
-        settingPage.logout();
     }
 
 }
